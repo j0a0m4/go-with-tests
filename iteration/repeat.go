65e5018c2ebd18b759f96ca2d98ca87b34fe05ef
João <joaomarcoslopes@id.uff.br>
2021-11-10 01:50:34 -0300

    feat: Repeat with benchmark

---
+++ iteration/repeat.go
@@ -0,0 +1,10 @@
+package iteration
+
+// Repeat takes a character and repeats it n times
+func Repeat(character string, n int) string {
+	var repeated string
+	for i := 0; i < n; i++ {
+		repeated += character
+	}
+	return repeated
+}
