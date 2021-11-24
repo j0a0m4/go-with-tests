54b01613bc518d85d59cbd70861289fc8f2adf29
João <joaomarcoslopes@id.uff.br>
2021-11-23 23:53:37 -0300

    feat: reflection with 2 str fields

---
+++ walk/walk_test.go
@@ -0,0 +1,46 @@
+package walk
+
+import (
+	"reflect"
+	"testing"
+)
+
+type TestCase struct {
+	Name          string
+	Input         interface{}
+	ExpectedCalls []string
+}
+
+func TestWalk(t *testing.T) {
+	cases := []TestCase{
+		{
+			"Struct with only one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
