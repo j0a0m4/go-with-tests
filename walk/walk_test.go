8f0e1837b39c0d90f24b51917aff2648cee7310c
João <joaomarcoslopes@id.uff.br>
2021-11-25 00:35:26 -0300

    feat: walk on nested structs

--- walk/walk_test.go
+++ walk/walk_test.go
@@ -11,6 +11,16 @@ type TestCase struct {
 	ExpectedCalls []string
 }
 
+type Person struct {
+	Name string
+	Profile Profile
+}
+
+type Profile struct {
+	Age int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 	cases := []TestCase{
 		{
@@ -26,6 +36,19 @@ func TestWalk(t *testing.T) {
 				Name string
 				City string
 			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with nested fields",
+			Person{"Chris", Profile{33, "London"}},
 			[]string{"Chris", "London"},
 		},
 	}
