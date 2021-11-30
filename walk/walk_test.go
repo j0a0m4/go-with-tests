733bee0f8f9665a6bbb714b614fc0296c34a7f11
João Lopes <joaomarcoslopes@id.uff.br>
2021-11-29 21:48:00 -0300

    feat: walk on slices

--- walk/walk_test.go
+++ walk/walk_test.go
@@ -12,44 +12,57 @@ type TestCase struct {
 }
 
 type Person struct {
-	Name string
+	Name    string
 	Profile Profile
 }
 
 type Profile struct {
-	Age int
+	Age  int
 	City string
 }
 
 func TestWalk(t *testing.T) {
 	cases := []TestCase{
 		{
-			"Struct with only one string field",
-			struct {
+			Name: "Struct with only one string field",
+			Input: struct {
 				Name string
 			}{"Chris"},
-			[]string{"Chris"},
+			ExpectedCalls: []string{"Chris"},
 		},
 		{
-			"Struct with two string fields",
-			struct {
+			Name: "Struct with two string fields",
+			Input: struct {
 				Name string
 				City string
 			}{"Chris", "London"},
-			[]string{"Chris", "London"},
+			ExpectedCalls: []string{"Chris", "London"},
 		},
 		{
-			"Struct with non string field",
-			struct {
+			Name: "Struct with non string field",
+			Input: struct {
 				Name string
-				Age int
+				Age  int
 			}{"Chris", 33},
-			[]string{"Chris"},
+			ExpectedCalls: []string{"Chris"},
 		},
 		{
-			"Struct with nested fields",
-			Person{"Chris", Profile{33, "London"}},
-			[]string{"Chris", "London"},
+			Name:          "Struct with nested fields",
+			Input:         Person{"Chris", Profile{33, "London"}},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
+		{
+			Name:          "Pointers to things",
+			Input:         &Person{"Chris", Profile{33, "London"}},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
+		{
+			Name: "Slices",
+			Input: []Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			ExpectedCalls: []string{"London", "Reykjavik"},
 		},
 	}
 
